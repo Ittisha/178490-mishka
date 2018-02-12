fb3d42eb2b65251fa59eb5e2c51cc7adf1e64db8
Anna Zezulkina <annzez@gmail.com>
2018-02-12 14:39:18 +0300

    Add gulp config, svg sprite, webp, modal close button

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,10 @@
+var ESC_CODE = 27;
+
 var mainNav = document.querySelector('.main-nav');
 var navButton = document.querySelector('.main-nav__toggle');
 var navButtonText = navButton.querySelector('span');
 var modal = document.querySelector('.modal');
+var modalCloseButton = modal.querySelector('.modal__close');
 var pageMain = document.querySelector('.page-main');
 var mapContainer = document.querySelector('.contacts__map');
 
@@ -31,29 +34,46 @@ if (navButton && mainNav && navButtonText) {
   navButton.addEventListener('click', onNavButtonClick);
 }
 
-if (pageMain && modal) {
+if (pageMain && modal && modalCloseButton) {
   var onModalButtonClick = function (event) {
     event.preventDefault();
 
-    if (event.target.classList.contains('promo-product__order') || event.target.classList.contains('product__order')) {
+    if (event.target.classList.contains('promo-product__order') || event.target.parentNode.classList.contains('product__order') || event.target.parentNode.classList.contains('product__icon-cart')) {
       modal.classList.remove('modal--hidden');
     }
   };
 
+  var modalClose = function () {
+    modal.classList.add('modal--hidden');
+  };
+
   var onModalOverlayClick = function (event) {
     if (event.target === modal) {
-      modal.classList.add('modal--hidden');
+      modalClose();
+    }
+  };
+
+  var onCloseButtonClick = function () {
+    event.preventDefault();
+    modalClose();
+  };
+
+  var onModalEscPress = function (event) {
+    if (event.keyCode === ESC_CODE) {
+      modalClose();
     }
   };
 
   pageMain.addEventListener('click', onModalButtonClick);
   modal.addEventListener('click', onModalOverlayClick);
+  modalCloseButton.addEventListener('click', onCloseButtonClick);
+  document.addEventListener('keydown', onModalEscPress);
 }
 
 if (mapContainer) {
   function initMap() {
     var center = {lat: 59.938660, lng: 30.323000};
-    var pin = "img/icon-map-pin.svg";
+    var pin = 'img/icon-map-pin.svg';
 
     var map = new google.maps.Map(mapContainer, {
       zoom: 16,
